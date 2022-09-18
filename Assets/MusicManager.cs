fd8ef965606e23b613a594fb789c60289ba82bcd
Fagin <hales.fagin@gmail.com>
2022-09-18 01:32:22 +0100

    misc

--- Assets/MusicManager.cs
+++ Assets/MusicManager.cs
@@ -8,7 +8,7 @@ public class MusicManager : MonoBehaviour
   private static MusicManager musicInstance;
   void Awake()
   {
-  DontDestroyOnLoad (this);
+  DontDestroyOnLoad(this);
 
   if (musicInstance == null) {
       musicInstance = this;
