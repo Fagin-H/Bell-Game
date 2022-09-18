7341a4b4ec4fb1a45aa791f4c584e6fa05eb36bf
Petra <petra.brcic@gmail.com>
2022-09-18 01:24:00 +0100

    added backgroud, added music (needs to be fixed)

---
+++ Assets/MusicManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+
+  private static MusicManager musicInstance;
+  void Awake()
+  {
+  DontDestroyOnLoad (this);
+
+  if (musicInstance == null) {
+      musicInstance = this;
+  } else {
+    Destroy(gameObject);
+  }
+}
+
+public static void SetVolume(System.Single volume)
+{
+  musicInstance.GetComponent<AudioSource>().volume = volume;
+}
+
+}
